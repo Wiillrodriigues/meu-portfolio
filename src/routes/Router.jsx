1c5b9c0186f8238d2cca45c2538b133166e3e3db
Will Rodrigues <wamaralr@gmail.com>
2026-05-27 12:19:00 -0300

    fix: corrige caminho do index.css na raiz do projeto

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,9 @@
 import { lazy, Suspense } from "react";
-import { createHashRouter } from "react-router-dom"; // Mudamos para HashRouter
+import { createHashRouter } from "react-router-dom"; 
 import Loading from "../components/common/loading/Loading";
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
-// Com o HashRouter, as rotas funcionam de forma relativa automática,
-// eliminando a necessidade do basename para o GitHub Pages!
 export const router = createHashRouter([
   {
     path: "/",
